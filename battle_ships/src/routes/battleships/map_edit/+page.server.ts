340e920e3f4bb3665f951ea151b5783e5be69add
nod3zer0 <game1men@gmail.com>
2024-10-17 22:40:56 +0200

    ship api connected

--- battle_ships/src/routes/battleships/map_edit/+page.server.ts
+++ battle_ships/src/routes/battleships/map_edit/+page.server.ts
@@ -1,10 +1,25 @@
 export async function load({ locals }) {
 
-let ships = [ { "name": "Carrier","placed": "false", "position": [0,0], "parts": [ [0,0], [0,1], [0,2], [0,3], [0,4] ] }, { "name": "Battleship","placed": "false", "position": [0,0], "parts": [ [0,0], [0,1], [0,2], [0,3] ] }, { "name": "Cruiser","placed": "false", "position": [0,0], "parts": [ [0,0], [0,1], [0,2] ] }, { "name": "Submarine","placed": "false", "position": [0,0], "parts": [ [0,0], [0,1], [0,2] ] }, { "name": "Destroyer","placed": "false", "position": [0,0], "parts": [ [0,0], [0,1] ] } ];
+//let ships = [ { "name": "Carrier", "position": [0,0], "parts": [ [0,0], [0,1], [0,2], [0,3], [0,4] ] }, { "name": "Battleship", "position": [0,0], "parts": [ [0,0], [0,1], [0,2], [0,3] ] }, { "name": "Cruiser", "position": [0,0], "parts": [ [0,0], [0,1], [0,2] ] }, { "name": "Submarine", "position": [0,0], "parts": [ [0,0], [0,1], [0,2] ] }, { "name": "Destroyer", "position": [0,0], "parts": [ [0,0], [0,1] ] } ];
+
+
+const ships_json = await fetch('http://127.0.0.1:8081/ships', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+let ships = await ships_json.json();
+
+console.log(ships_json);
+
+console.log(ships);
 
 ships.forEach(ship => {
     ship.bitmap = createBitmap(ship, 5);
 });
+
 
 
 return {
